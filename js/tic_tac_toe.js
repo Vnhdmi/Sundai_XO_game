c2006f5fa82db447bc376679147a9051c1c44e3f
s247027 <caovanhieu34@gmail.com>
2026-01-22 14:49:08 +0900

    05-07.game_7.verup1

--- js/tic_tac_toe.js
+++ js/tic_tac_toe.js
@@ -10,7 +10,6 @@ let counter = 9;
 const squares = document.getElementsByClassName("square");
 
 // Array に変換
-// https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Array/from
 const squaresArray = Array.from(squares);
 
 // squaresの要素を取得
@@ -23,8 +22,6 @@ const b_3 = document.getElementById("b_3");
 const c_1 = document.getElementById("c_1");
 const c_2 = document.getElementById("c_2");
 const c_3 = document.getElementById("c_3");
-
-// NewGameボタン取得
 
 // Win or Lose Judgment Line
 const line1 = JudgLine(squaresArray, ["a_1", "a_2", "a_3"]);
@@ -41,131 +38,116 @@ const lineArray = [line1, line2, line3, line4, line5, line6, line7, line8];
 let winningLine = null;
 
 // メッセージ
-const msgtxt1 = '<p class="image"><img src="img/penguins.jpg" width=61px height=61px></p><p class="text">Penguins Attack!</p>';
-const msgtxt2 = '<p class="image"><img src="img/whitebear.jpg" width=61px height=61px></p><p class="text">WhiteBear Attack!</p>';
+const msgtxt1 = '<p class="image"><img src="img/penguins.jpg" width=61px height=61px></p><p class="text">Penguins Attack! (your turn)</p>';
+const msgtxt2 = '<p class="image"><img src="img/whitebear.jpg" width=61px height=61px></p><p class="text">WhiteBear Attack! (computer turn)</p>';
 const msgtxt3 = '<p class="image"><img src="img/penguins.jpg" width=61px height=61px></p><p class="text animate__animated animate__lightSpeedInRight">Penguins Win!!</p>';
 const msgtxt4 = '<p class="image"><img src="img/whitebear.jpg" width=61px height=61px></p><p class="text animate__animated animate__lightSpeedInLeft">WhiteBear Win!!</p>';
 const msgtxt5 = '<p class="image"><img src="img/penguins.jpg" width=61px height=61px><img src="img/whitebear.jpg" width=61px height=61px></p><p class="text animate__bounceIn">Draw!!</p>';
 
-//sound effect
-let gameSound = ["sound/click_sound1.mp3","sound/click_sound2.mp3","sound/penwin_sound.mp3","sound/bearwin_sound.mp3","sound/draw_sound.mp3"];
-// ページ本体が読み込まれたタイミングで実行するコード
+// sound effect
+let gameSound = ["sound/click_sound1.mp3", "sound/click_sound2.mp3", "sound/penwin_sound.mp3", "sound/bearwin_sound.mp3", "sound/draw_sound.mp3"];
+
+// 本体が読み込まれたタイミングで実行がるコード
 window.addEventListener("DOMContentLoaded",
     function() {
-        // メッセージ(最初はpenguinsのターンから)
+        // メッセージ (最初はpenguins ターンから)
         setMessage("pen-turn");
+        // squareがクリック可能かを判断するクラスを追加
+        squaresArray.forEach(function(square) {
+            square.classList.add("js-clickable");
+        });
     }, false
 );
 
 // Win or Lose Judgment Lineを配列化
-// JavaScriptでfilterを使う方法: https://techacademy.jp/magazine/15575
 function JudgLine(targetArray, idArray) {
     return targetArray.filter(function(e) {
         return (e.id === idArray[0] || e.id === idArray[1] || e.id === idArray[2]);
     });
 }
 
-// squareをクリックしたときにイベント発火
-// クリックしたsquareに、penguinsかbearを表示。画像を表示したsquareはクリックできないようにする
-// Win or Lose Judgementの呼び出し
+// squareをクリックしたときにイベント発火(はっか)
+// クリックしたsquareに、penguinsかbearを表示。画像を表示した
+squaresArray.forEach(function(square) {
+    square.addEventListener('click', () => {
+        let gameOverFlg = isSelect(square);
 
-a_1.addEventListener("click",
-    function() {
-        isSelect(a_1);
-    }, false
-);
+        // GameOverではない場合、クマのターン (auto)
+        if (gameOverFlg === "0") {
+            const squaresBox = document.getElementById("squaresBox");
+            squaresBox.classList.add("js-unclickable");
 
-a_2.addEventListener("click", () => {
-    isSelect(a_2);
-});
-
-a_3.addEventListener("click", () => {
-    isSelect(a_3);
-});
-
-b_1.addEventListener("click", () => {
-    isSelect(b_1);
-});
-
-b_2.addEventListener("click", () => {
-    isSelect(b_2);
-});
-
-b_3.addEventListener("click", () => {
-    isSelect(b_3);
-});
-
-c_1.addEventListener("click", () => {
-    isSelect(c_1);
-});
-
-c_2.addEventListener("click", () => {
-    isSelect(c_2);
-});
-
-c_3.addEventListener("click", () => {
-    isSelect(c_3);
+            setTimeout(
+                function() {
+                    bearTurn();
+                },
+                "2000"
+            );
+        }
+    });
 });
 
 // クリックしたsquareにはpenguinsかbearを表示。
-// ・表示したところはクリックできないようにする。
 function isSelect(selectSquare) {
+    let gameOverFlg = "0";
 
     if (flag === "pen-flag") {
+        let music = new Audio(gameSound[0]);
+        music.currentTime = 0;
+        music.play();
 
-    // click sound ef
-    let music = new Audio(gameSound[0]);
-    music.currentTime = 0;
-    music.play();
-        // Penguins turn
         selectSquare.classList.add("js-pen-checked");
         selectSquare.classList.add("js-unclickable");
+        // squareがクリック可能かを判断するクラスを削除
+        selectSquare.classList.remove("js-clickable");
 
         // penguins win
         if (isWinner("penguins")) {
             setMessage("pen-win");
-            gameOver("penguins"); // A-1
-            return;
+            gameOver("penguins");
+            return gameOverFlg = "1";
         }
 
         setMessage("bear-turn");
         flag = "bear-flag";
 
     } else {
-        // click sound ef
         let music = new Audio(gameSound[1]);
         music.currentTime = 0;
         music.play();
-        // Bear turn
+
         selectSquare.classList.add("js-bear-checked");
         selectSquare.classList.add("js-unclickable");
+        // squareがクリック可能かを判断するクラスを削除
+        selectSquare.classList.remove("js-clickable");
 
         // white-bear win
         if (isWinner("bear")) {
             setMessage("bear-win");
-            gameOver("bear"); // A-2
-            return;
+            gameOver("bear");
+            return gameOverFlg = "1";
         }
 
         setMessage("pen-turn");
         flag = "pen-flag";
     }
 
-    // ターン数カウンターを-1する
+    // カウンター
     counter--;
 
-    // ターン数=0になったらDRAW
+    // ターン数 l=0 になったらDRAW
     if (counter === 0) {
         setMessage("draw");
-        gameOver("draw"); // A-3
+        gameOver("draw");
+        return gameOverFlg = "1";
     }
+
+    return gameOverFlg = "0";
 }
 
 // 勝敗判定
-// classListの使い方まとめ: https://qiita.com/tomokichi_ruby/items/2460c5902d19b81cace5
 function isWinner(symbol) {
     const result = lineArray.some(function(line) {
-        // every: 全て条件を満たしていればTrueを返す
         const subResult = line.every(function(square) {
             if (symbol === "penguins") {
                 return square.classList.contains("js-pen-checked");
@@ -174,11 +156,9 @@ function isWinner(symbol) {
                 return square.classList.contains("js-bear-checked");
             }
         });
-        // trueを返したlineをwinningLineに代入
         if (subResult) {
             winningLine = line
         }
-
         return subResult;
     });
     return result;
@@ -202,14 +182,11 @@ function setMessage(id) {
         case "draw":
             document.getElementById("msgtext").innerHTML = msgtxt5;
             break;
-        default:
-            document.getElementById("msgtext").innerHTML = msgtxt1;
     }
 }
 
 // ゲーム終了時の処理
-function gameOver(status) { //
-    //Game over sound ef
+function gameOver(status) {
     let w_sound;
     switch (status) {
         case "penguins":
@@ -225,75 +202,74 @@ function gameOver(status) { //
     let music = new Audio(w_sound);
     music.currentTime = 0;
     music.play();
-    // all square unclickable
-    squaresArray.forEach(function(square) { // B
-        square.classList.add("js-unclickable");
-    });
 
-    // winEffect
-    if (status === "penguins") {
-        // winner-line penguins high-light
-        if (winningLine) {
-            winningLine.forEach(function(square) { // C-1
-                square.classList.add("js-pen_highLight");
-            });
-        }
-        // penguins win!! ==>snow color is pink
-        $(document).snowfall({
-            flakeColor: "rgb(255,240,245)", // 雪の色
-            maxSpeed: 3, // 最大速度
-            minSpeed: 1, // 最小速度
-            maxSize: 20, // 最大サイズ
-            minSize: 10, // 最小サイズ
-            round: true // 雪の形を丸にする
-        });
+    // squares-boxをクリックできないようにする
+    const squaresBox = document.getElementById("squaresBox");
+    squaresBox.classList.add("js-unclickable");
 
-    } else if (status === "bear") {
-        // winner-line bear high-light
-        if (winningLine) {
-            winningLine.forEach(function(square) { // C-2
-                square.classList.add("js-bear_highLight");
-            });
-        }
-        // whitebear win!! ==>snow color is blue
-        $(document).snowfall({
-            flakeColor: "rgb(175,238,238)", // 雪の色
-            maxSpeed: 3, // 最大速度
-            minSpeed: 1, // 最小速度
-            maxSize: 20, // 最大サイズ
-            minSize: 10, // 最小サイズ
-            round: true // 雪の形を丸にする
-        });
+    if (status === "penguins" && winningLine) {
+        winningLine.forEach(square => square.classList.add("js-pen_highLight"));
+        $(document).snowfall({ flakeColor: "rgb(255,240,245)", maxSpeed: 3, minSpeed: 1, maxSize: 20, minSize: 10, round: true });
+    } else if (status === "bear" && winningLine) {
+        winningLine.forEach(square => square.classList.add("js-bear_highLight"));
+        $(document).snowfall({ flakeColor: "rgb(175,238,238)", maxSpeed: 3, minSpeed: 1, maxSize: 20, minSize: 10, round: true });
     }
     newgamebtn_display.classList.remove("js-hidden");
 }
+
 /************************************************************/
 /* New Game ボタン                                          */
 /************************************************************/
-// NewGameボタン クリック時、ゲーム初期化
 const newgamebtn_display = document.getElementById("newgame-btn");
 const newgamebtn = document.getElementById("btn90");
 
-//Display NewGameボタン
-
-
-newgamebtn.addEventListener("click", function(){
-    //pengunis btn
+// NewGameボタン クリック時、ゲーム初期化
+newgamebtn.addEventListener("click", function() {
+    // penguins ターン
     flag = "pen-flag";
 
-    //ターン数カウンター
+    // ターン数カウンター
     counter = 9;
 
     winningLine = null;
-    squaresArray.forEach(function(square){
+    squaresArray.forEach(function(square) {
         square.classList.remove("js-pen-checked");
         square.classList.remove("js-bear-checked");
         square.classList.remove("js-unclickable");
         square.classList.remove("js-pen_highLight");
         square.classList.remove("js-bear_highLight");
+        // マス目に、クリック可能かを判断するクラスを追加
+        square.classList.add("js-clickable");
     });
+
+    // マス目のエリアのプロテクトを解除
+    const squaresBox = document.getElementById("squaresBox");
+    squaresBox.classList.remove("js-unclickable");
+
     setMessage("pen-turn");
     newgamebtn_display.classList.add("js-hidden");
-    //stop snowfall
+    // snowfall stop
     $(document).snowfall("clear");
 });
+
+// クマのターン
+function bearTurn() {
+    let gameOverFlg = "0";
+
+    // クリックできるマス目を抽出する。
+    const bearSquare = squaresArray.filter(function(square) {
+        return square.classList.contains("js-clickable");
+    });
+
+    // クリックできるマス目の中から、1つをランダムに選ぶ。
+    let n = Math.floor(Math.random() * bearSquare.length);
+
+    // 選択したマス目の処理
+    gameOverFlg = isSelect(bearSquare[n]);
+
+    // GameOverではない場合、マス目のエリアをクリックできるようにする。
+    if (gameOverFlg === "0") {
+        const squaresBox = document.getElementById("squaresBox");
+        squaresBox.classList.remove("js-unclickable");
+    }
+}
